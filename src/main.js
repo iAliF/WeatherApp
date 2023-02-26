50da088a9c72a712929fc2951f5d24da58b93391
Ali <Mr.Ali.Fotouhi@gmail.com>
2023-02-26 22:04:29 +0330

    Lesson 2 & 3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import './assets/tailwind.css'
 
 const app = createApp(App)
 
